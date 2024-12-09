d9bdcbfbde8cf692c0af49476b715bf6a257f6b3
Clepher Jaudian <clepher.jaudian@msunaawan.edu.ph>
2024-12-10 06:22:55 +0800

    using ziggy routes in primevue

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { Link } from '@inertiajs/vue3';
 import { ref } from 'vue';
 
 // Ref
@@ -10,7 +11,7 @@ const sideBarItems = ref([
     {
         label: 'Dashboard',
         icon: 'pi pi-objects-column',
-        // route: '/home'
+        route: route('home')
     },
     {
         label: 'Messages',
@@ -141,13 +142,11 @@ const menuBarItems = ref([
                     </span>
                 </template>
                 <template #item="{ item, props, hasSubmenu }">
-                    <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-                        <a v-ripple :href="href" v-bind="props.action" @click="navigate">
-                            <span :class="item.icon" />
-                            <span class="ml-2">{{ item.label }}</span>
-                            <Badge v-if="item.badge" class="ml-auto" :value="item.badge" />
-                        </a>
-                    </router-link>
+                    <Link v-if="item.route" v-ripple :href="item.route" class="flex items-center cursor-pointer text-surface-700 dark:text-surface-0 px-4 py-2">
+                        <span :class="item.icon" />
+                        <span class="ml-2">{{ item.label }}</span>
+                        <Badge v-if="item.badge" class="ml-auto" :value="item.badge" />
+                    </Link>
                     <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
                         <span :class="item.icon" />
                         <span class="ml-2">{{ item.label }}</span>
@@ -156,24 +155,8 @@ const menuBarItems = ref([
                 </template>
                 <!-- <template #submenulabel="{ item }">
                     <span class="text-primary font-bold">{{ item.label }}</span>
-                </template>
-                <template #item="{ item, props }">
-                    <a v-ripple class="flex items-center" v-bind="props.action">
-                        <span :class="item.icon" />
-                        <span>{{ item.label }}</span>
-                        <Badge v-if="item.badge" class="ml-auto" :value="item.badge" />
-                        <span v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{ item.shortcut }}</span>
-                    </a>
-                </template> -->
-                <!-- <template #end>
-                    <button v-ripple class="relative overflow-hidden w-full border-0 bg-transparent flex items-start p-2 pl-4 hover:bg-surface-100 dark:hover:bg-surface-800 rounded-none cursor-pointer transition-colors duration-200">
-                        <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" class="mr-2" shape="circle" />
-                        <span class="inline-flex flex-col items-start">
-                            <span class="font-bold">Amy Elsner</span>
-                            <span class="text-sm">Admin</span>
-                        </span>
-                    </button>
-                </template> -->
+                </template>-->
+                
             </TieredMenu>
             </div>
         </aside>
@@ -182,37 +165,28 @@ const menuBarItems = ref([
             <!-- Main Content -->
             <main class="flex-1">
                 <header class="fixed top-0 left-60 right-0 z-[1100]">
-                    <!-- <nav> -->
-                        <!-- <div class="space-x-6">
-                            <Link :href="route('home')" class="nav-link">Home</Link>
-                        </div>
-
-                        <div class="space-x-6">
-                            <Link :href="route('login')" class="nav-link">Login</Link>
-                            <Link :href="route('register')" class="nav-link">Register</Link>
-                        </div> -->
-                        <Menubar :model="menuBarItems" class="w-full md:w-100 !border-l-0 !rounded-none">
-                            <template #start>
-                                <span class="pi pi-bars"></span>
-                            </template>
-                            <template #item="{ item, props, hasSubmenu, root }">
-                                <a v-ripple class="flex items-center" v-bind="props.action">
-                                    <span>{{ item.label }}</span>
-                                    <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge" />
-                                    <span v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{ item.shortcut }}</span>
-                                    <i v-if="hasSubmenu" :class="['pi pi-angle-down ml-auto', { 'pi-angle-down': root, 'pi-angle-right': !root }]"></i>
-                                </a>
-                            </template>
-                            <template #end>
-                                <button v-ripple class="relative overflow-hidden w-full border-0 bg-transparent flex items-start justify-center pl-4 hover:bg-surface-100 dark:hover:bg-surface-800 rounded-none cursor-pointer transition-colors duration-200">
-                                    <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" class="mr-2" shape="circle" />
-                                    <span class="inline-flex flex-col items-start">
-                                        <span class="font-bold text-xs">Amy Elsner</span>
-                                        <span class="text-xs">Admin</span>
-                                    </span>
-                                </button>
-                            </template>
-                        </Menubar>
+                    <Menubar :model="menuBarItems" class="w-full md:w-100 !border-l-0 !rounded-none">
+                        <template #start>
+                            <span class="pi pi-bars"></span>
+                        </template>
+                        <template #item="{ item, props, hasSubmenu, root }">
+                            <a v-ripple class="flex items-center" v-bind="props.action">
+                                <span>{{ item.label }}</span>
+                                <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge" />
+                                <span v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{ item.shortcut }}</span>
+                                <i v-if="hasSubmenu" :class="['pi pi-angle-down ml-auto', { 'pi-angle-down': root, 'pi-angle-right': !root }]"></i>
+                            </a>
+                        </template>
+                        <template #end>
+                            <button v-ripple class="relative overflow-hidden w-full border-0 bg-transparent flex items-start justify-center pl-4 hover:bg-surface-100 dark:hover:bg-surface-800 rounded-none cursor-pointer transition-colors duration-200">
+                                <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" class="mr-2" shape="circle" />
+                                <span class="inline-flex flex-col items-start">
+                                    <span class="font-bold text-xs">Amy Elsner</span>
+                                    <span class="text-xs">Admin</span>
+                                </span>
+                            </button>
+                        </template>
+                    </Menubar>
                     <!-- </nav> -->
                 </header>
 
