7f65b176c1addb8e1b26affd0e333d5b01bcd29a
Nick Joey Cabahug <nickjoey.cabahug@msunaawan.edu.ph>
2026-01-13 09:43:59 +0800

    User seeder update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,35 +4,51 @@ namespace Database\Seeders;
 
 use App\Models\Role;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
+        /** ADMIN USER */
         $adminUser = User::create([
             'name' => 'Admin User',
-            'email' => 'admin@1',
-            'password' => '123',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('123'),
         ]);
+
         $adminRole = Role::where('name', 'Admin')->first();
         if ($adminRole) {
-            $adminUser->roles()->attach($adminRole);
+            $adminUser->roles()->attach($adminRole->id);
         }
 
+        /** TOURNAMENT MANAGERS */
+        $tmUsers = [
+            [
+                'name' => 'TM User',
+                'email' => 'sports1@example.com',
+                'password' => '123',
+            ],
+            [
+                'name' => 'TM User2',
+                'email' => 'sports2@example.com',
+                'password' => '123',
+            ],
+        ];
 
-        $tmUser = User::create([
-            'name' => 'TM User',
-            'email' => 'sports@1',
-            'password' => '123',
-        ]);
         $tmRole = Role::where('name', 'TournamentManager')->first();
-        if ($tmRole) {
-            $tmUser->roles()->attach($tmRole);
+
+        foreach ($tmUsers as $userData) {
+            $tmUser = User::create([
+                'name' => $userData['name'],
+                'email' => $userData['email'],
+                'password' => Hash::make($userData['password']),
+            ]);
+
+            if ($tmRole) {
+                $tmUser->roles()->attach($tmRole->id);
+            }
         }
     }
 }
